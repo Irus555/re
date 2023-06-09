dd2abbedc171807e909f4e77019ddb95e092bbeb
Irus555 <135014847+Irus555@users.noreply.github.com>
2023-06-09 14:30:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
-//const [name,setName]=useState()
-// useEffect(()=>{}) срабатывает при загрузке и по окончанию
-//name-наименование состояния; setName-метод обновления состояния
-// Устаревший вариант, классовый компонент
-// class Welcome extends React.Component {
-// 	constructor(props) {
-// 		super(props)
-// 		this.state = {
-// 			name: 'Volodya'
-// 		}
-// 	}
 
-
-// 	render() {
-// 		const updateName = (name) => {
-// 			const state = this.state
-// 			this.setState({ ...state, name: name })
-// 		}
-// 		return (
-// 			<>
-// 				<p>{this.state.name}</p>
-// 				<button type="" onClick={() => updateName('yyy')}>Upd name</button>
-// 			</>
-// 		)
-// 	}
-// }
-// export default Welcome;
-
-
-// function App() {
-// 	const [name, setName] = useState('Valera')
-// 	const updateName = (name) => {
-// 		setName(name)
-// 	}
-
-// 	const getData = async () => {
-// 		let data = await fetch('https://jsonplaceholder.typicode.com/users')
-// 			.then(res => res.json())
-// 			.then(res => res)
-// 		console.log('res', data)
-// 	}
-// 	getData()
-// 	return (
-// 		<>
-// 			<p style={{ color: 'blue', fontSize: '24px' }}> {name} </p>
-// 			<button type="" onClick={() => { updateName('xxx') }}>Update name</button>
-// 		</>
-// 	)
-// }
-
-// export default App;
 function App() {
 
 	const [users, setUsers] = useState([])
